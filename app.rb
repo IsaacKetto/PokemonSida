9d3a306ea6f59eedbc7b061a37fe59d9faf4e13f
IsaacKetto <gylleneisaac@gmail.com>
2023-03-10 16:15:49 +0100

    random shit

--- app.rb
+++ app.rb
@@ -10,12 +10,27 @@ enable :sessions
 db = SQLite3::Database.new('db/database.db')
 db.results_as_hash = true
 
+before do
+    @type_colors = TypeColours
+end
+
 get '/' do
     if session[:logged_in]
         random_number = rand(1..151)
         @pokemon = db.execute('SELECT * FROM pokemons WHERE id=?', random_number).first
+        session[:pokemon] = @pokemon
     end
     slim(:index)
+end
+
+post '/catch' do
+    catch_pokemon(session[:current_user][:user_id], session[:pokemon]["id"], db)   
+    redirect('/')
+end
+
+get '/inventory' do
+    @pokemons = db.execute('SELECT * FROM pokemons WHERE id IN (SELECT pokemon_id FROM user_pokemon_relation WHERE user_id = ?)', session[:current_user][:user_id])
+    slim(:"inventory/index")
 end
 
 get '/pokemon' do
@@ -66,10 +81,23 @@ post '/login' do
     if BCrypt::Password.new(password_digest) == password
         flash[:notice] = "Successful login"
         session[:logged_in] = true
-        session[:current_user] = {username: username}
+        session[:current_user] = {
+            username: username, 
+            user_id: db.execute('SELECT user_id FROM users WHERE username=? LIMIT 1', username).first["user_id"]
+        }
         redirect '/'
     else 
         flash[:notice] = "Incorrect password or username!"
         redirect('/login')
     end
+end
+
+post '/pokemon/type' do
+    if params["type"] == "all"
+        @pokemons = db.execute('SELECT * FROM pokemons')
+    else
+        @pokemons = db.execute('SELECT * FROM pokemons WHERE type_1=? OR type_2=?', params["type"].capitalize, params["type"].capitalize)
+    end
+    @filter = params["type"]
+    slim(:"pokemon/index")
 end
--- lib/module.rb
+++ lib/module.rb
@@ -13,47 +13,31 @@ def special_name(name)
     return pokemon_name
 end
 
+def catch_pokemon(user_id, pokemon_id, db)
+    db.execute('INSERT INTO user_pokemon_relation (user_id, pokemon_id) VALUES (?, ?)', user_id, pokemon_id)
+end
+
 def pokemon_data(db, id)
     return db.execute('SELECT * FROM pokemons WHERE id=?', id)
 end
 
-def type_color(type)
-    case type
-    when "normal"
-        return "#aa9"
-    when "fire"
-        return "#FF4422"
-    when "water"
-        return "#3399FF"
-    when "electric"
-        return "#FFCC33"
-    when "grass"
-        return "#77CC55"
-    when "ice"
-        return "#66CCFF"
-    when "fighting"
-        return "#BB5544"
-    when "poison"
-        return "#AA5599"
-    when "ground"
-        return "#DDBB55"
-    when "flying"
-        return "#8899FF"
-    when "psychic"
-        return "#FF5599"
-    when "bug"
-        return "#AABB22"
-    when "rock"
-        return "#BBAA66"
-    when "ghost"
-        return "#6666BB"
-    when "dragon"
-        return "#7766EE"
-    when "dark"
-        return "#775544"
-    when "steel"
-        return "#AAAABB"
-    when "fairy"
-        return "#EE99EE"
-    end
-end
+TypeColours = {
+    normal: "#aa9",
+    fire: "#FF4422",
+    water: "#3399FF",
+    electric: "#FFCC33",
+    grass: "#77CC55",
+    ice: "#66CCFF",
+    fighting: "#BB5544",
+    poison: "#AA5599",
+    ground: "#DDBB55",
+    flying: "#8899FF",
+    psychic: "#FF5599",
+    bug: "#AABB22",
+    rock: "#BBAA66",
+    ghost: "#6666BB",
+    dragon: "#7766EE",
+    dark: "#775544",
+    steel: "#AAAABB",
+    fairy: "#EE99EE"
+}
