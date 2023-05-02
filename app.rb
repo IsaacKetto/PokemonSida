5c1f6c8fd1c9642475c052da0525b954c3eb6e1f
IsaacKetto <gylleneisaac@gmail.com>
2023-05-02 19:21:38 +0200

    KLAR

--- app.rb
+++ app.rb
@@ -5,8 +5,6 @@ require 'bcrypt'
 require 'slim'
 require 'json'
 require 'sinatra/flash'
-require 'yardoc'
-require 'yard-sinatra'
 Dir[File.dirname(__FILE__) + '/lib/*.rb'].each {|file| require file }
 
 enable :sessions
@@ -25,7 +23,8 @@ before do
     end
 end
 
-# Displays 
+# Display Landing page
+#
 get '/' do
     if session[:logged_in] == true
         random_number = rand(1..151)
@@ -35,21 +34,34 @@ get '/' do
     slim(:index)
 end
 
+# Catches pokemon by insterting its data into user_pokemon_relation and redirects back to landing route
+#
+# @see Model#catch_pokemon
 post '/catch' do
     catch_pokemon(session[:current_user][:user_id], session[:pokemon]["id"])   
     redirect('/')
 end
 
+# Displays current user's inventory
+#
+# @see Model#fetch_inventory
 get '/inventory' do
     @pokemons, @relation_data = fetch_inventory(session[:current_user][:user_id])
     slim(:"inventory/index")
 end
 
+# Displays a user's teams
+#
+# @see Model#fetch_teams
 get '/team' do
     @teams = fetch_teams(session[:current_user])
     slim(:"team/index")
 end
 
+# Displays an admin page containing every user logged in the databse
+#
+# @see Model#all_users
+# @see Model#admin_check
 get '/admin/users' do
     if admin_check(session[:current_user][:user_id])
         @users = all_users()
@@ -57,6 +69,10 @@ get '/admin/users' do
     end
 end
 
+# Displays an admin page containing every team logged in the database
+#
+# @see Model#admin_check
+# @see Model#all_teams
 get '/admin/teams' do
     if admin_check(session[:current_user][:user_id])
         @teams = all_teams()
@@ -64,6 +80,11 @@ get '/admin/teams' do
     end
 end
 
+# Displays am admin edit form to change a user's team
+#
+# @see Model#admin_check
+# @see Model#fetch_user_id_from_team
+# @see Model#fetch_inventory
 get '/admin/teams/:id/edit' do
     if admin_check(session[:current_user][:user_id])
         @team_id = params[:id]
@@ -76,6 +97,12 @@ get '/admin/teams/:id/edit' do
     end
 end
 
+# Updates a teams information and redirects to admin/teams route
+#
+# @param pokemon [Array] A list containing pokemon data
+# @param id [Integer] An id to indentify the correct team
+# @see Model#admin_check
+# @see Model#update_team
 post '/admin/teams/:id/update' do
     if params[:pokemons] != nil
         if admin_check(session[:current_user][:user_id])
@@ -88,6 +115,11 @@ post '/admin/teams/:id/update' do
     redirect(back)
 end
 
+# Deletes data from user_team_relation table and redirects to admin/teams route
+#
+# @param id [Integer] An id to identify the correct team
+# @see Model#admin_check
+# @see Model#delete_team
 post '/admin/teams/:id/delete' do
     if admin_check(session[:current_user][:user_id])
         delete_team(params[:id])
@@ -97,17 +129,30 @@ post '/admin/teams/:id/delete' do
     redirect('/admin/teams')
 end
 
+# Displays an edit form to change a team's information
+#
+# @param id [Integer] An id to identify the correct team
+# @see Model#fetch_inventory
 get '/team/:id/edit' do
     @your_pokemons, @relation_data = fetch_inventory(session[:current_user][:user_id])
     @team_id = params[:id]
     slim(:"/team/edit")
 end
 
+# Displays all existing teams from the databse
+#
+# @see Model#all_teams
 get '/showcase' do
     @teams = all_teams()
     slim(:showcase)
 end
 
+# Updates a team's information and redirects to /team route
+#
+# @param id [Integer] An id to identity the correct team
+# @param pokemons [Array] A list containing pokemon data
+# @see Model#correct_user_team?
+# @see Model#update_team
 post '/team/:id/update' do
     if params[:pokemons] != nil
         if correct_user_team?(session[:current_user][:user_id], params[:id])
@@ -122,16 +167,26 @@ post '/team/:id/update' do
     redirect(back)
 end
 
+# Displays a form to create a new team
+#
+# @see Model#fetch_inventory
 get '/team/new' do
     @your_pokemons, @relation_data = fetch_inventory(session[:current_user][:user_id])
     slim(:"team/new")
 end
 
+# Displays all pokemons
+#
+# @see Model#fetch_pokemons
 get '/pokemon' do
     @pokemons = fetch_pokemons(nil)
     slim(:"pokemon/index")
 end    
 
+# Displays pokemons that have the same type as the input
+#
+# @param type [String] The type to check
+# @see Model#type_fetch
 get '/pokemon/type/:type' do |type|
 
     if type == "all"
@@ -144,19 +199,29 @@ get '/pokemon/type/:type' do |type|
     slim(:'pokemon/index')
 end
 
+# Displays a specific pokemons data
+#
+# @param id [Integer] The unique id of the pokemon
+# @see Model#pokemon_data
 get '/pokemon/:id' do
     @pokemon = pokemon_data(params[:id]).first
     slim(:"pokemon/show")
 end
 
+# Displays a signup form
+#
 get '/signup' do
     slim(:signup)
 end
 
+# Displays a login form
+#
 get '/login' do
     slim(:login)
 end
 
+# Logs out user by deleting sessions and redirects to Landing route
+#
 get '/logout' do
     session.delete(:current_user)
     session[:logged_in] = false
@@ -164,6 +229,11 @@ get '/logout' do
     redirect('/')
 end
 
+# Updates a user's information and redirects to admin/users route
+#
+# @param id [Integer] The id of the user
+# @see Model#update_user
+# @see Model#admin_check
 post '/admin/users/:id/update' do
     if admin_check(session[:current_user][:user_id])
         update_user(params["username"], params[:id])
@@ -171,6 +241,11 @@ post '/admin/users/:id/update' do
     redirect(:'/admin/users')
 end
 
+# Deletes a user from the database and redirects to admin/users
+#
+# @param id [Integer] The id of the user
+# @see Model#admin_check
+# @see Model#delete_user
 post '/admin/users/:id/delete' do
     if admin_check(params[:id])
         flash[:notice] = "You cannot delete the Admin user"
@@ -187,22 +262,33 @@ post '/admin/users/:id/delete' do
     redirect(:"admin/users")
 end
 
+# Creates a new team and redirects back to team route
+#
+# @param pokemons [Array] List containing pokemon data
+# @param team_name [String] The name of the team
+# @see Model#create_team
 post '/team' do
     if params[:pokemons] == nil || params["team_name"] == ""
         flash[:notice] = "You didnt select pokemons or didnt add a team name"
         redirect(back)
-    elsif params[:pokemons] != nil || params["team_name"] != ""
+    elsif params[:pokemons] != nil && params["team_name"] != ""
         selected_pokemons = JSON.generate(params[:pokemons].map { |value| JSON.parse(value) })
         create_team(selected_pokemons, session[:current_user][:user_id], params["team_name"])
         redirect(:"/team")
     end
 end
 
+# Adds a new user in the databse and redirects to login route
+# 
+# @param username [String] The username of the user
+# @param password [String] The password, obviously
+# @param password_repeat [String] Password, again
+# @see Model#signup
 post '/signup' do
   username = params[:username]
   password = params[:password]
   password_repeat = params[:password_repeat]
-  if password.length < 5
+  if password.length < 3
     flash[:notice] = "Password is too short"
   else
     if password == password_repeat
@@ -216,6 +302,13 @@ post '/signup' do
 end
 
 login_attemps = {}
+# Attemps to login a user
+#
+# @param username [String] The username
+# @param password [String] The password
+# @see Model#fetch_row
+# @see Model#user_exists?
+# @see Model#crypt_password
 post '/login' do
     username = params[:username]
     password = params[:password]
@@ -246,7 +339,11 @@ post '/login' do
     redirect('/login')
 end
 
-# fix secured delete
+# Deletes a pokemon from a user's inventory and redirects back to previous page
+#
+# @param id [Integer] The id of the pokemon
+# @see Model#correct_user_inventory?
+# @see Model#delete_pokemon_from_inv
 post '/pokemon/:id/delete' do
     if correct_user_inventory?(session[:current_user][:user_id], params[:id])
         delete_pokemon_from_inv(params[:id])
@@ -256,6 +353,9 @@ post '/pokemon/:id/delete' do
     redirect('/')
 end
 
+# Filters the "all pokemon" display by type and redirects to pokemon/type/:type route
+#
+# @param type [String] The type to filter for
 post '/pokemon/type' do
     type = params["type"]
     redirect("/pokemon/type/#{type}")
--- lib/model.rb
+++ lib/model.rb
@@ -1,6 +1,5 @@
 $db = SQLite3::Database.new('db/database.db')
 $db.results_as_hash = true
-
 # Fetch pokemons from database
 #
 # @param id [Integer] The id of a pokemon
@@ -12,7 +11,6 @@ def fetch_pokemons(id)
         return $db.execute('SELECT * FROM pokemons WHERE id=?', id).first
     end
 end
-
 # Inserts relation ids in user_pokemon_relation table in $db database
 #
 # @param user_id [Integer] Id of the user
@@ -21,7 +19,6 @@ end
 def catch_pokemon(user_id, pokemon_id)
     $db.execute('INSERT INTO user_pokemon_relation (user_id, pokemon_id) VALUES (?, ?)', user_id, pokemon_id)
 end
-
 # Deletes user corresponding to the param user_id's value thorugh a CASCADE ON DELETE format
 #
 # @param user_id [Integer] Id of the user
@@ -31,7 +28,6 @@ def delete_user(user_id)
     $db.execute('DELETE FROM user_pokemon_relation WHERE user_id=?', user_id)
     $db.execute('DELETE FROM user_team_relation WHERE user_id=?', user_id)
 end
-
 # Checks if current user is the same as the user corresponding to the team
 #
 # @param user_id [Integer] Id of the user
@@ -40,7 +36,6 @@ end
 def correct_user_team?(user_id, unique_id)
     return $db.execute('SELECT user_id FROM user_team_relation WHERE team_id=?', unique_id).first["user_id"] == user_id
 end
-
 # Checks if current user is the same as the user corresponding to the inventory
 #
 # @param user_id [Integer] Id of the user
@@ -49,7 +44,6 @@ end
 def correct_user_inventory?(user_id, unique_id)
     return $db.execute('SELECT user_id FROM user_pokemon_relation WHERE id=?', unique_id).first["user_id"] == user_id
 end
-
 # Updates a users name
 #
 # @param new_name [String] The new name
@@ -58,7 +52,6 @@ end
 def update_user(new_name, id)
     $db.execute("UPDATE users SET username=? WHERE user_id=?", new_name, id)
 end
-
 # Encrypts a password for security
 #
 # @param password_digest [String] The password to encrypt
@@ -66,7 +59,6 @@ end
 def crypt_password(password_digest)
     return BCrypt::Password.new(password_digest)
 end
-
 # Checks if user exists
 #
 # @param username [String] The users profile name
@@ -74,7 +66,6 @@ end
 def user_exists?(username)
     return !$db.execute('SELECT * FROM users WHERE username=?', username).empty?
 end
-
 # Fetches pokemons that have the same type as the param type
 #
 # @param type [String] The pokemon type
@@ -82,7 +73,6 @@ end
 def type_fetch(type)
     return $db.execute('SELECT * FROM pokemons WHERE type_1=? OR type_2=?', type.capitalize, type.capitalize)
 end
-
 # Fetches encrypted password from the user
 #
 # @param username [String] The profile name of the user
@@ -90,7 +80,6 @@ end
 def fetch_row(username)
     return $db.execute("SELECT password FROM users WHERE username=?", username).first
 end
-
 # Fetches user_id
 #
 # @param username [String] The username of the user
@@ -98,7 +87,6 @@ end
 def fetch_user_id(username)
     return $db.execute('SELECT user_id FROM users WHERE username=? LIMIT 1', username).first["user_id"]
 end
-
 # Inserts data into the user_team_relation table
 #
 # @param pokemons [String] A list of pokemon data
@@ -108,7 +96,6 @@ end
 def create_team(pokemons, user_id, team_name)
     $db.execute('INSERT INTO user_team_relation (user_id, pokemons, team_name) VALUES (?,?,?)', user_id, pokemons, team_name)
 end
-
 # Creates a new user by inserting data into users table
 #
 # @param username [String] The username of the new user
@@ -118,7 +105,6 @@ def signup(username, password)
     password_digest = BCrypt::Password.create(password)
     $db.execute("INSERT INTO users (username, password) VALUES (?,?)", username, password_digest)
 end
-
 # Updates user_team_relation data 
 #
 # @param new_name [String] The new name
@@ -132,7 +118,6 @@ def update_team(new_name, pokemon_list, team_id)
         $db.execute('UPDATE user_team_relation SET pokemons=?, team_name=? WHERE team_id=?', pokemon_list, new_name, team_id)
     end
 end
-
 # Deletes table instances from user_team_relation
 # 
 # @param team_id [Integer] An Id to determine which team to delete
@@ -140,7 +125,6 @@ end
 def delete_team(team_id)
     $db.execute('DELETE FROM user_team_relation WHERE team_id=?', team_id)
 end
-
 # Fetches data from the user_pokemon_relation table
 # 
 # @param user_id [Integer] An id to fetch correct data
@@ -154,14 +138,12 @@ def fetch_inventory(user_id)
     relation_data = $db.execute('SELECT pokemon_id, id FROM user_pokemon_relation WHERE user_id=?', user_id)
     return pokemons, relation_data
 end
-
 # Fetches all data from the user_team_relation table
 #
 # @return [Array] A list containing the relation data
 def all_teams()
     return $db.execute('SELECT * FROM user_team_relation')
 end
-
 # Fetches user_id from user_team_relation table
 # 
 # @param id [Integer] An id to fetch correct data
@@ -169,7 +151,6 @@ end
 def fetch_user_id_from_team(id)
     return $db.execute('SELECT user_id FROM user_team_relation WHERE team_id=?', id).first["user_id"]
 end
-
 # Deletes instance of data from the user_pokemon_relation table
 #
 # @param id [Integer] An id to determine which instance to delete
@@ -177,14 +158,12 @@ end
 def delete_pokemon_from_inv(id)
     $db.execute('DELETE FROM user_pokemon_relation WHERE id=?', params[:id])
 end
-
 # Fetches all data from the users table
 # 
 # @return [Array] A list containing user data
 def all_users()
     return $db.execute('SELECT * FROM users')
 end
-
 # fetches a user's teams
 #
 # @param user [Hashmap] A user's data
@@ -192,7 +171,6 @@ end
 def fetch_teams(user)
     $db.execute('SELECT pokemons, team_name, team_id FROM user_team_relation INNER JOIN users ON user_team_relation.user_id = users.user_id AND users.user_id=?', user[:user_id])
 end
-
 # Fetches a pokemon's data
 #
 # @param id [Integer] An id to determine which pokemon to fetch data from
@@ -200,7 +178,6 @@ end
 def pokemon_data(id)
     return $db.execute('SELECT * FROM pokemons WHERE id=?', id)
 end
-
 # Checks if the user is an administrator
 #
 # @param user_id [Integer] The current user's id
